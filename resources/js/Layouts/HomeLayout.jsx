3afad32cd3acf3f806a237321b44f8bd953245de
etienne gomez <etiennez0r@gmail.com>
2022-10-06 19:07:10 -0400

    Implemented historical data display in the front end

--- resources/js/Layouts/HomeLayout.jsx
+++ resources/js/Layouts/HomeLayout.jsx
@@ -6,7 +6,7 @@ const HomeLayout = ({children}) => {
     <>
         <NavBar />
 
-        <main className="flex items-center justify-center h-screen -mt-20">
+        <main className="flex items-center justify-center">
             {children}
         </main>
 
--- resources/js/Pages/Historical.jsx
+++ resources/js/Pages/Historical.jsx
@@ -1,12 +1,40 @@
 import HomeLayout from '@/Layouts/HomeLayout'
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import {API_ENDPOINT, SYMBOL} from '@/utils'
 
 const Home = () => {
+  const [rows, setRows] = useState([])
+
+  useEffect(() => {
+    axios.get(`${API_ENDPOINT}/historical?symbol=${SYMBOL}`).then((result) => {
+      setRows(result.data.rows)
+    })
+  }, [])
+
   return (
     <HomeLayout>
       <div className="content">
-        HISTORICAL...
+        <table class="w-full">
+          <thead>
+            <tr className="border border-gray-400">
+              <th>ID</th>
+              <th>Price</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              rows.map((row) => {
+                return (<tr key={row.id}>
+                  <td>{row.id}</td>
+                  <td>{row.price}</td>
+                  <td>{row.time}</td>
+                </tr>)
+              })
+            }
+          </tbody>
+        </table>
       </div>
     </HomeLayout>
   )
